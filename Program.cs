09fb0d4005f1a2952a29ac96de73565c845ba1af
Igor Alekseev <alekseevi15@gmail.com>
2014-11-20 13:07:57 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using StructureMap.Pipeline;
+
+namespace MysteryWithDynamic
+{
+    class Program
+    {
+        private class A { }
+
+        private static LambdaInstance<object> Method(A obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void Main(string[] args)
+        {
+            var instance = (dynamic)new A();
+            Method(instance); //(*)hangs forever on this line                    
+        }
+    }
+}
